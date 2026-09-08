7e3196193997bd6a4644610c41d54a76eff88760
Shuanglei Tao <tsl0922@gmail.com>
2026-09-08 16:10:09 +0800

    remove redundant else block

--- GUI/Adafruit_GFX.c
+++ GUI/Adafruit_GFX.c
@@ -137,16 +137,10 @@ void GFX_firstPage(Adafruit_GFX* gfx) {
 bool GFX_nextPage(Adafruit_GFX* gfx, buffer_callback callback, void* user_data) {
     if (callback) {
         int16_t page_ys = gfx->current_page * gfx->page_height;
-        if (gfx->px != 0 || gfx->py != 0 || gfx->pw != gfx->_width || gfx->ph != gfx->_height) {
-            int16_t page_ye = gfx->current_page < gfx->total_pages - 1 ? page_ys + gfx->page_height : gfx->HEIGHT;
-            uint16_t dest_ys = gfx->py + page_ys;  // transposed
-            uint16_t dest_ye = MIN(gfx->py + gfx->ph, gfx->py + page_ye);
-            if (dest_ye > dest_ys)
-                callback(user_data, gfx->buffer, gfx->color, gfx->px, dest_ys, gfx->pw, dest_ye - dest_ys);
-        } else {
-            int16_t height = MIN(gfx->page_height, gfx->HEIGHT - page_ys);
-            callback(user_data, gfx->buffer, gfx->color, 0, page_ys, gfx->WIDTH, height);
-        }
+        int16_t page_ye = gfx->current_page < gfx->total_pages - 1 ? page_ys + gfx->page_height : gfx->HEIGHT;
+        uint16_t dest_ys = gfx->py + page_ys;  // transposed
+        uint16_t dest_ye = MIN(gfx->py + gfx->ph, gfx->py + page_ye);
+        if (dest_ye > dest_ys) callback(user_data, gfx->buffer, gfx->color, gfx->px, dest_ys, gfx->pw, dest_ye - dest_ys);
     }
 
     gfx->current_page++;
